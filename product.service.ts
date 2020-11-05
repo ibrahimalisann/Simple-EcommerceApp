9053db95f66688beaf63a98756a2405621e8e8d5
ibrahimalisann <37002649+ibrahimalisann@users.noreply.github.com>
2020-11-05 22:05:16 +0300

    Add files via upload

---
+++ product.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Product } from '../product/product';
+import { from, Observable, throwError } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
+
+@Injectable()
+export class ProductService {
+  constructor(private http: HttpClient) {}
+  path = 'http://localhost:3000/products';
+
+  getProducts(categoryId): Observable<Product[]> {
+    let newPath = this.path;
+    if (categoryId) {
+      newPath += '?categoryId=' + categoryId;
+    }
+    return this.http.get<Product[]>(newPath).pipe(
+      tap((data) => console.log(JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+  }
+  addProduct(product: Product): Observable<Product> {
+    const httpOptions={
+      headers:new HttpHeaders({
+        'Contant-Type':'application/json',
+        'Authorization':'Token' 
+      })
+    }
+    return this.http.post<Product>(this.path, product,httpOptions).pipe(
+      tap((data) => console.log(JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+  }
+
+  handleError(err: HttpErrorResponse) {
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = 'There is someting wrong' + err.error.message;
+    } else {
+      errorMessage = "A System's Error";
+    }
+    return throwError(errorMessage);
+  }
+}
